6cbe24e6854ed5108468c1e7664ec9040621f08a
Kostya <kostyash2009@mail.ru>
2024-05-16 20:02:00 +0300

    new database table, new page - view tasks, where you can see your tasks

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ def login():
     if request.method == 'POST':
         log = request.form['username']
         password = request.form['password']
-        con = sqlite3.connect('users.db')
+        con = sqlite3.connect('project.db')
         cur = con.cursor()
         print(request.form['username'], request.form['password'])
         query = f'''SELECT * FROM users WHERE login = "{log}" AND password = "{password}"'''
         data = cur.execute(query).fetchall()
         if data:
-            return redirect(url_for('home'))
+            return redirect(url_for('home', username=log))
         else:
             return {'anwser': 'Ошибка. Неверный логин/пароль.'}
     else:
@@ -39,7 +39,7 @@ def register():
     if request.method == 'POST':
         log = request.form['username']
         password = request.form['password']
-        con = sqlite3.connect('users.db')
+        con = sqlite3.connect('project.db')
         cur = con.cursor()
         query = cur.execute(f'''SELECT login FROM users WHERE login = "{log}"''').fetchall()
         if len(query) == 1:
@@ -51,19 +51,31 @@ def register():
             cur.execute(query)
             con.commit()
             con.close()
-            return redirect(url_for('home'))
+            return redirect(url_for('home', username=log))
     else:
         return render_template('register.html')
 
 
-@server.route('/home', methods=['GET', 'POST'])
-def home():
+@server.route('/home/<username>/', methods=['GET', 'POST'])
+def home(username):
     if request.method == 'POST':
         if list(request.form.values())[0] == '1':
-            return redirect(url_for('create_projects'))
+            return redirect(url_for('create_projects', username=username))
         elif list(request.form.values())[0] == '2':
-            return redirect(url_for('view_projects'))
+            return redirect(url_for('view_projects', username=username))
     else:
         return render_template('home.html')
+
+
+@server.route('/view_projects/<username>/', methods=['GET', 'POST'])
+def view_projects(username):
+    if request.method == 'POST':
+        pass
+    else:
+        con = sqlite3.connect('project.db')
+        cur = con.cursor()
+        query = cur.execute(f'''SELECT task, description_task FROM tasks WHERE login="{username}"''').fetchall()
+        return render_template('view_tasks.html', query=query, username=username)
+
 
 server.run()
